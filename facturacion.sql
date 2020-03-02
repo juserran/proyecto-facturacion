56c8721c25ada57992fe716c4d3747cd92d2fb65
juserran <jserrano231@gmail.com>
2020-03-02 13:23:18 +0100

    BBDD con procedimientos almacenados

--- facturacion.sql
+++ facturacion.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `facturacion` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `facturacion`;
 -- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
 --
 -- Host: localhost    Database: facturacion
@@ -18,32 +16,32 @@ USE `facturacion`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `cliente_proveedor`
+-- Table structure for table `cliente_proovedor`
 --
 
-DROP TABLE IF EXISTS `cliente_proveedor`;
+DROP TABLE IF EXISTS `cliente_proovedor`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
-CREATE TABLE `cliente_proveedor` (
+CREATE TABLE `cliente_proovedor` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nif` varchar(9) NOT NULL,
   `nombre` varchar(20) NOT NULL,
   `direccion` varchar(90) DEFAULT NULL,
   `tipo` char(1) NOT NULL,
   `email` varchar(20) DEFAULT NULL,
-  `telefono` varchar(9) NOT NULL,
+  `telefono` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `cliente_proveedor`
+-- Dumping data for table `cliente_proovedor`
 --
 
-LOCK TABLES `cliente_proveedor` WRITE;
-/*!40000 ALTER TABLE `cliente_proveedor` DISABLE KEYS */;
-INSERT INTO `cliente_proveedor` VALUES (1,'12345678A','Maria','san jose','c','mjh@hhh','12345678'),(2,'12345678B','Pedro','san Bartolo','p','mjnh@hygv','123456789');
-/*!40000 ALTER TABLE `cliente_proveedor` ENABLE KEYS */;
+LOCK TABLES `cliente_proovedor` WRITE;
+/*!40000 ALTER TABLE `cliente_proovedor` DISABLE KEYS */;
+INSERT INTO `cliente_proovedor` VALUES (1,'12345678A','María San José','C/ San José, 8','C','sanjose12@gmail.com',654456654),(2,'12345678B','Pedro Gil','','C','',653353354),(3,'12345678B','Manuel García','C/ Ferraz, 7','P','garcia22@gmail.com',653213994),(4,'12345678Z','Juanfran','','C','',659998877);
+/*!40000 ALTER TABLE `cliente_proovedor` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -60,8 +58,8 @@ CREATE TABLE `contrato` (
   `encargado_facturacion` char(1) NOT NULL,
   PRIMARY KEY (`id_contrato`),
   KEY `fk_contrato_cliente` (`id`),
-  CONSTRAINT `fk_contrato_cliente` FOREIGN KEY (`id`) REFERENCES `cliente_proveedor` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `fk_contrato_cliente` FOREIGN KEY (`id`) REFERENCES `cliente_proovedor` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,7 +68,7 @@ CREATE TABLE `contrato` (
 
 LOCK TABLES `contrato` WRITE;
 /*!40000 ALTER TABLE `contrato` DISABLE KEYS */;
-INSERT INTO `contrato` VALUES (1,2,25,'p');
+INSERT INTO `contrato` VALUES (1,3,10.5,'E'),(2,3,8.2,'E'),(3,1,9,'F'),(4,2,7.5,'F'),(5,1,2,'E');
 /*!40000 ALTER TABLE `contrato` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,22 +86,22 @@ CREATE TABLE `facturas` (
   `fecha` date NOT NULL,
   `id_servicio` int(11) NOT NULL,
   `importe` double NOT NULL,
-  `id_forrma_pago` int(11) NOT NULL,
+  `id_forma_pago` int(11) NOT NULL,
   `id_contrato` int(11) DEFAULT NULL,
   `concepto` varchar(90) DEFAULT NULL,
   `iva` double NOT NULL,
   PRIMARY KEY (`id_facturas`),
   KEY `fk_factura_cliente` (`id`),
-  KEY `fk_factura_servicio` (`id_servicio`),
-  KEY `fk_factura_pago` (`id_forrma_pago`),
-  KEY `fk_factura_contrato` (`id_contrato`),
   KEY `fk_factura_pedido` (`id_pedido`),
-  CONSTRAINT `fk_factura_cliente` FOREIGN KEY (`id`) REFERENCES `cliente_proveedor` (`id`),
+  KEY `fk_factura_servicio` (`id_servicio`),
+  KEY `fk_factura_pago` (`id_forma_pago`),
+  KEY `fk_factura_contrato` (`id_contrato`),
+  CONSTRAINT `fk_factura_cliente` FOREIGN KEY (`id`) REFERENCES `cliente_proovedor` (`id`),
   CONSTRAINT `fk_factura_contrato` FOREIGN KEY (`id_contrato`) REFERENCES `contrato` (`id_contrato`),
-  CONSTRAINT `fk_factura_pago` FOREIGN KEY (`id_forrma_pago`) REFERENCES `forma_pago` (`id_forma_pago`),
+  CONSTRAINT `fk_factura_pago` FOREIGN KEY (`id_forma_pago`) REFERENCES `forma_pago` (`id_forma_pago`),
   CONSTRAINT `fk_factura_pedido` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`),
   CONSTRAINT `fk_factura_servicio` FOREIGN KEY (`id_servicio`) REFERENCES `servicios_productos` (`id_servicio`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -112,6 +110,7 @@ CREATE TABLE `facturas` (
 
 LOCK TABLES `facturas` WRITE;
 /*!40000 ALTER TABLE `facturas` DISABLE KEYS */;
+INSERT INTO `facturas` VALUES (1,1,1,'2020-01-07',1,3000,2,1,'Pagina SEO/SEM',21),(2,2,2,'2020-02-01',3,5000,3,2,'SEO/SEM análisis',21),(3,3,3,'2020-02-17',2,1000,1,3,'Página',21);
 /*!40000 ALTER TABLE `facturas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -127,7 +126,7 @@ CREATE TABLE `forma_pago` (
   `descripcion` varchar(90) NOT NULL,
   `valor` double NOT NULL,
   PRIMARY KEY (`id_forma_pago`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -136,7 +135,7 @@ CREATE TABLE `forma_pago` (
 
 LOCK TABLES `forma_pago` WRITE;
 /*!40000 ALTER TABLE `forma_pago` DISABLE KEYS */;
-INSERT INTO `forma_pago` VALUES (1,'pago unico',0);
+INSERT INTO `forma_pago` VALUES (1,'Pago en 3 meses',3),(2,'Pago en 6 meses',6),(3,'Pago en 9 meses',9);
 /*!40000 ALTER TABLE `forma_pago` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -162,7 +161,7 @@ CREATE TABLE `pedido` (
   KEY `fk_pedido_formapago` (`id_forma_pago`),
   KEY `fk_pedido_servicios` (`id_servicio`),
   KEY `fk_pedido_contrato` (`id_contrato`),
-  CONSTRAINT `fk_pedido_cliente` FOREIGN KEY (`id`) REFERENCES `cliente_proveedor` (`id`),
+  CONSTRAINT `fk_pedido_cliente` FOREIGN KEY (`id`) REFERENCES `cliente_proovedor` (`id`),
   CONSTRAINT `fk_pedido_contrato` FOREIGN KEY (`id_contrato`) REFERENCES `contrato` (`id_contrato`),
   CONSTRAINT `fk_pedido_formapago` FOREIGN KEY (`id_forma_pago`) REFERENCES `forma_pago` (`id_forma_pago`),
   CONSTRAINT `fk_pedido_servicios` FOREIGN KEY (`id_servicio`) REFERENCES `servicios_productos` (`id_servicio`)
@@ -175,7 +174,7 @@ CREATE TABLE `pedido` (
 
 LOCK TABLES `pedido` WRITE;
 /*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
-INSERT INTO `pedido` VALUES (1,'pagina basica',1,1,120000,'2015-02-12',1,NULL,12000);
+INSERT INTO `pedido` VALUES (1,'Solicitan pagina simple SEO y SEM',1,2,3000,'2020-01-07',1,1,1000),(2,'Solicitan SEO/SEM y análisis',2,3,5000,'2020-02-01',3,2,500),(3,'Solicitan página simple',3,1,1000,'2020-02-17',2,3,0);
 /*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -194,8 +193,8 @@ CREATE TABLE `servicios_productos` (
   `iva` double NOT NULL,
   PRIMARY KEY (`id_servicio`),
   KEY `fk_servicios_cliente` (`id`),
-  CONSTRAINT `fk_servicios_cliente` FOREIGN KEY (`id`) REFERENCES `cliente_proveedor` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `fk_servicios_cliente` FOREIGN KEY (`id`) REFERENCES `cliente_proovedor` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -204,18 +203,14 @@ CREATE TABLE `servicios_productos` (
 
 LOCK TABLES `servicios_productos` WRITE;
 /*!40000 ALTER TABLE `servicios_productos` DISABLE KEYS */;
-INSERT INTO `servicios_productos` VALUES (1,'pagina basica',12000,1,21);
+INSERT INTO `servicios_productos` VALUES (1,'Pagina simple',1000,1,21),(2,'SEO/SEM',2000,2,21),(3,'Análisis',5000,3,21);
 /*!40000 ALTER TABLE `servicios_productos` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Dumping events for database 'facturacion'
---
-
---
 -- Dumping routines for database 'facturacion'
 --
-/*!50003 DROP PROCEDURE IF EXISTS `altaPedido` */;
+/*!50003 DROP PROCEDURE IF EXISTS `altaClienteProovedor` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
@@ -225,13 +220,27 @@ UNLOCK TABLES;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `altaPedido`(pid_pedido int,pdescripcion varchar(90),pid int(11), pid_forma_pago int(11),ptotal double,pfecha date,pid_servicio int(11),pid_contrato int(11),pimporte_facturado double)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `altaClienteProovedor`(pnif varchar(9), pnombre varchar(20), pdireccion varchar(90), ptipo char(1), pemail varchar(20), ptelefono int)
 BEGIN
-insert into pedido
-(id_pedido,descripcion,id,id_forma_pago,total,fecha,id_servicio,id_contrato,importe_facturado
+insert into cliente_proovedor
+(
+id,
+nif,
+nombre,
+direccion,
+tipo,
+email,
+telefono
 )
-values(pid_pedido,pdescripcion,pid , pid_forma_pago ,ptotal ,pfecha,pid_servicio ,pid_contrato ,pimporte_facturado
-
+values
+(
+0,
+pnif,
+pnombre,
+pdireccion,
+ptipo,
+pemail,
+ptelefono
 );
 END ;;
 DELIMITER ;
@@ -239,7 +248,7 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `borrarPedido` */;
+/*!50003 DROP PROCEDURE IF EXISTS `altaContrato` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
@@ -249,10 +258,162 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `borrarPedido`(pid_pedido int)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `altaContrato`(pid_contrato int, pid int, pcomision double, pencargado char(1))
 BEGIN
-delete from pedido
-where id_pedido=pid_pedido;
+insert into contrato
+(
+id_contrato, id, comision, encargado_facturacion)
+values
+(
+0, pid, pcomision, pencargado 
+);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `altaFormaPago` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `altaFormaPago`(pid int, pdescripcion varchar(90), pvalor double)
+BEGIN
+insert into forma_pago
+(id_forma_pago, descripcion, valor)
+values
+(
+0, pdescripcion, pvalor
+);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `bajaClienteProovedor` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `bajaClienteProovedor`(pid int)
+BEGIN
+delete from cliente_proovedor
+where id = pid;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `bajaContrato` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `bajaContrato`(pid int)
+BEGIN
+delete from contrato
+where id_contrato = pid;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `bajaFormaPago` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `bajaFormaPago`(pid int)
+BEGIN
+delete from forma_pago
+where id_forma_pago = pid;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `mostrarUnoClienteProovedor` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `mostrarUnoClienteProovedor`(pid int)
+BEGIN
+select id, nif, nombre, direccion, tipo, email, telefono
+from cliente_proovedor
+where pid = id;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `mostrarUnoContrato` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `mostrarUnoContrato`(pid int)
+BEGIN
+select id_contrato, id, comision, encargado_facturacion
+from contrato
+where pid = id_contrato;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `mostrarUnoFormaPago` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `mostrarUnoFormaPago`(pid int)
+BEGIN
+select id_forma_pago, descripcion, valor
+from forma_pago
+where pid = id_forma_pago;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -271,9 +432,9 @@ DELIMITER ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `mostrarUnoPedido`(pid int)
 BEGIN
-select id_pedido,descripcion,id,id_forma_pago,total,fecha,id_servicio,id_contrato,importe_facturado
+Select id_pedido, descripcion, id, id_forma_pago, total, fecha, id_servicio, id_contrato, importe_facturado
 from pedido
-where id_pedido=pid;
+where pid = id_pedido;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -290,4 +451,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-27 14:19:32
+-- Dump completed on 2020-02-28 14:02:19
